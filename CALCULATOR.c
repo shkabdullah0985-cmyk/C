35d5f4a634a61c129ab7051d54eac556c4138b1c
shkabdullah0985-cmyk <shkabdullah0985@gmail.com>
2025-11-07 22:27:50 +0530

    Refactor calculator to use float and add operations

--- CALCULATOR.c
+++ CALCULATOR.c
@@ -1,36 +1,66 @@
 #include <stdio.h>
+#include <math.h>
+
 int main() {
-    double a,b;
+
+    float number1, number2;
+   
     char operation;
-    printf("enter a number");
-    scanf("%lf",&a);
-    printf("enter a operation");
-    scanf("%lf",&operation);
-    printf("enter a number");
-    scanf("%lf",&b);
-     
-    double add,sub,mul,div;
-    add=a+b;
-    sub=a-b;
-    mul=a*b;
-    div=a/b;
-    if(operation=='+') {
-printf("addition is ",add);
+
+    printf("ENTER A NUMBER 1:- ");
+    scanf("%f", &number1);
+
+    printf("ENTER A NUMBER 2:- ");
+    scanf("%f", &number2);
+
+   
+    printf("ENTER AN OPERATION (+,-,*,/,%%,@,#,$) :- ");
+    scanf(" %c", &operation);
+
+    
+    if (operation == '+') {
+        printf("ADDITION IS :- %f\n", number1 + number2);
     } 
-    else if(operation=='-'){
-printf("subtraction is ",sub);
-    }
-    else if(operation=='*') {
-        printf("multiplication is", mul);
-    }
-    else if(operation=='/'){
-        if(b==0){
-            printf("enter a number except 0");
+    else if (operation == '-') {
+        printf("SUBTRACTION IS :- %f\n", number1 - number2);
+    } 
+    else if (operation == '*') {
+        printf("MULTIPLICATION IS :- %f\n", number1 * number2);
+    } 
+    else if (operation == '/') {
+        if (number2 == 0) {
+            printf("CANNOT DIVIDE BY 0.\n");
+        } else {
+            printf("DIVISION IS :- %f\n", number1 / number2);
         }
-        else {
-            printf("division is ",div);
+    } 
+    else if (operation == '%') {
+        if (number2 == 0) {
+            printf("CANNOT DIVIDE BY 0.\n");
+        } else {
+            printf("REMAINDER IS :- %f\n", fmod(number1, number2));
         }
-    }else {
-        printf("enter a valid operation");}
+    } 
+    else if (operation == '@') {
+        if (number2 == 0) {
+            printf("CANNOT DIVIDE BY ZERO.\n");
+        } else {
+            printf("PERCENTAGE IS :- %f%%\n", (number1 / number2) * 100);
+        }
+    } 
+    else if (operation == '#') {
+        printf("POWER IS :- %f\n", pow(number1, number2));
+    } 
+    else if (operation == '$') {
+        if (number1 < 0) {
+            printf("CANNOT TAKE SQUARE ROOT OF A NEGATIVE NUMBER.\n");
+        } else {
+            printf("SQUARE ROOT IS :- %f\n", sqrt(number1));
+        }
+    } 
+    else {
+        printf("ENTER A VALID OPERATION.\n");
+    }
+
 return 0;
-    }
+}
